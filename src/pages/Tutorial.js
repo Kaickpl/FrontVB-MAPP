b874812738718e7f1c4cdbf7ca85c04c0c075678
Kaickpl <kaick.mlopes@upe.br>
2025-11-30 07:23:49 -0300

    Ajuste de algumas classes

--- src/pages/Tutorial.js
+++ src/pages/Tutorial.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Card} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -109,10 +108,6 @@ export default function Tutorial() {
                   Voltar ao Login
                 </Link>
               </div>
-              
-
-              
-            
       </Container>
     </div>
   );
